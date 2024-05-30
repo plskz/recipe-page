1beb32c54e34762d444861fb1d46c529f0ee8822
Zai Santillan <zaisantillan14@gmail.com>
2024-05-30 08:45:36 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className='flex flex-col items-center justify-center min-h-screen'>
+      <p className='text-3xl'>Hello, World!</p>
+    </main>
+  )
+}
